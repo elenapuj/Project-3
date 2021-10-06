358acf1d24505444100e7bfcd53e036fefd6235f
elenapuj <89639906+elenapuj@users.noreply.github.com>
2021-10-06 16:36:55 +0200

    Add files via upload

---
+++ Particle.hpp
@@ -0,0 +1,42 @@
+//Here we define the "Particle" class
+
+#ifndef __LotteryBall_hpp__
+#define __LotteryBall_hpp__
+
+
+#include <armadillo>
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace arma;
+using namespace std;
+
+
+class Particle{
+
+	public:
+
+	  //First we declare the member variables
+
+	  double q_, m_;
+	  vec r_, v_;
+
+
+	  //Then, we declare also the constructor
+
+	  Particle(double q_in, double m_in, vec r_in, vec v_in);
+
+
+	  //Finally, we declare some methods so we are able to print everything later if needed
+
+	  void q();
+	  void m();
+	  void r();
+	  void v();
+	  void info();
+
+};
+
+
+#endif
