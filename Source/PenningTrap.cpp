7cbf18f58a6a7f073951406d11db0122d6d03fc0
mariaperez01 <89605350+mariaperez01@users.noreply.github.com>
2021-10-19 09:26:08 +0200

    Update PenningTrap.cpp

--- Source/PenningTrap.cpp
+++ Source/PenningTrap.cpp
@@ -26,7 +26,7 @@ void PenningTrap::add_particle(Particle particle_in){
 	
 }
 
-//Method that adds n particles using a loop
+//Method that adds n particles
 void PenningTrap::add_n_particles(int n , Particle particle_in ){
 	
 	for (int i = 1; i <= n; i++){
@@ -39,7 +39,8 @@ void PenningTrap::add_n_particles(int n , Particle particle_in ){
 
 //Method that calculates the external electric field
 void PenningTrap::external_E_field(vec& E, int i, double t, double f, double w, bool time_dependence){
-
+	
+	//The calculation of the external electric field varies depending on whether it depends on time or not
 	if (time_dependence){
 
 		E[0] = v0_ * ( 1 + f * cos( w * t ) ) *  particle_collection[i].r_[0]  / ( d_ * d_ );
@@ -62,14 +63,14 @@ void PenningTrap::external_E_field(vec& E, int i, double t, double f, double w, 
 
 }
 
-//Method that gives the external magnetic field
+//Method that gives the value of the external magnetic field
 void PenningTrap::external_B_field(vec& B){
 
         B[2] = b0_;
 
 }
 
-//Method that calculates the force between two particles  
+//Method that calculates the force between two particles, i.e. the Coulomb force 
 void PenningTrap::force_particle(vec& f_p, int i, int j){
 
         f_p = 1.38935333*pow(10,5)*particle_collection[j].q_*particle_collection[i].q_*(particle_collection[i].r_-particle_collection[j].r_)/(pow(norm(particle_collection[i].r_-particle_collection[j].r_),3));
@@ -96,8 +97,7 @@ void PenningTrap::total_force_particle(vec& t_f_p, int i){
 
 //Method that calculates the total external force, i.e. the Lorentz force, to which the particle is subjected
 void PenningTrap::total_force_external(vec& t_f_e, int i, double t, double f, double w, bool time_dependence){
-	
-	//We define a vector E with three components
+
 	vec E(3);
 
 	external_E_field(E, i, t, f, w, time_dependence);
@@ -113,11 +113,11 @@ void PenningTrap::total_force_external(vec& t_f_e, int i, double t, double f, do
 
 //Method that calculates the sum of all forces
 void PenningTrap::total_force(vec& t_f, int i, double t, double f, double w, bool null_exterior, bool time_dependence, bool coulomb){
-
+	//The calculation of the sum of all forces depends on wether the external fields E and B are zero or not
 	if (null_exterior){
 
 		if( norm(particle_collection[i].r_) < d_ ){
-
+			//The caclutation of the total force also depends if the Coulomb interactions are taking or not into account
 			if (coulomb){
 
                         	vec t_f_e(3);
@@ -201,8 +201,7 @@ void PenningTrap::total_force(vec& t_f, int i, double t, double f, double w, boo
 
 //Method that evolves the PenningTrap system in time using the forward Euler method
 void PenningTrap::evolve_forward_Euler(double dt, int i, double t, double f, double w, bool null_exterior, bool time_dependence, bool coulomb) {
-	
-	//We define a vector t_f with three components
+
 	vec t_f(3);
 
 	total_force(t_f, i, t, f, w, null_exterior, time_dependence, coulomb);
@@ -232,7 +231,6 @@ void PenningTrap::evolve_RK4(double dt, int i, double t, double f, double w, boo
 	//This vector represents the acceleration of the particle
 	vec a = t_f / particle_collection[i].m_;
 
-
 	vec K1r = v_old * dt;
 	vec K1v = a * dt;
 
@@ -250,19 +248,19 @@ void PenningTrap::evolve_RK4(double dt, int i, double t, double f, double w, boo
         vec K2r = particle_collection[i].v_ * dt;
         vec K2v = a * dt;
 
-
+	//We assign the new values to the position and velocity of the particle
         particle_collection[i].r_ = r_old + K2r / 2;
         particle_collection[i].v_ = v_old + K2v / 2;
 
         total_force(t_f, i, t + dt/2, f, w, null_exterior, time_dependance, coulomb);
 
+	//Now we calculate the new value of the acceleration of the particle
         a = t_f / particle_collection[i].m_;
-
 
         vec K3r = particle_collection[i].v_ * dt;
         vec K3v = a * dt;
 
-
+	//We assign the new values to the position and velocity of the particle
         particle_collection[i].r_ = r_old + K3r;
         particle_collection[i].v_ = v_old + K3v;
 
@@ -270,7 +268,6 @@ void PenningTrap::evolve_RK4(double dt, int i, double t, double f, double w, boo
         total_force(t_f, i, t + dt, f, w, null_exterior, time_dependance, coulomb);
 
         a = t_f / particle_collection[i].m_;
-
 
         vec K4r = particle_collection[i].v_ * dt;
         vec K4v = a * dt;
