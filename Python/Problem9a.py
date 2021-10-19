ed9c392c2b27ccdf77702264b84bceba4c79e4f0
mariaperez01 <89605350+mariaperez01@users.noreply.github.com>
2021-10-19 10:24:01 +0200

    Update Problem9a.py

--- Python/Problem9a.py
+++ Python/Problem9a.py
@@ -9,6 +9,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 
+"""
+The code opens two data files, creates three vectors for each file, and fills them with the three 
+components of the position and velocity of the particle respectively
+"""
 with  open('Problem9a_Rrk.txt', "r") as  infile:
     
     lines = infile.readlines ()
@@ -35,7 +39,8 @@ with  open('Problem9a_Vrk.txt', "r") as  infile:
         vx.append(float(vals1 [0]))
         vy.append(float(vals1 [1]))
         vz.append(float(vals1 [2]))
-        
+ 
+#Plot
 h = 0.1
 tf = 100        
 t = np.linspace(0 , tf, 1002 )
@@ -44,4 +49,6 @@ plt.title("Motion of one particle in the z direction vs time", fontsize=10)
 plt.ylabel("z (μm)" )
 plt.xlabel("t (μs)")
 plt.grid(True) #Grids get painted
+
+#The graph is saved in a PDF file
 plt.savefig("Problem_9a_plot.pdf")
