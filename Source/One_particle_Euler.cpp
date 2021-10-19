1414e656e4729fe6739f42891817cb84ed23ff44
elenapuj <89639906+elenapuj@users.noreply.github.com>
2021-10-19 19:57:23 +0200

    Update One_particle_Euler.cpp

--- Source/One_particle_Euler.cpp
+++ Source/One_particle_Euler.cpp
@@ -15,23 +15,23 @@ int main(){
 	PenningTrap my_trap( 96.5, 9.65 * pow(10 , 8), 10000 , my_particle_collection);
 	
 	
+	//We define some other data for the penning trap
 	
-	
-	double f = 0;  
+	double f = 0;  //As this is only necessary when the electric field is time dependent, we define it as zero  
 
-	double w = 0;
+	double w = 0;  //Same as the one before
 
 	double h = 0.1; //Stepsize
 
 	double t = 0; //Initial time
 
 	double tf = 100; //Final time
-
-	vec r0 = vec(3).randn() * 0.1 * my_trap.d_;  // random initial position
- 	vec v0 = vec(3).randn() * 0.1 * my_trap.d_;  // random initial velocity
-
+	
 	
 	//We create a particle by calling the Particle constructor
+
+	vec r0 = vec(3).randn() * 0.1 * my_trap.d_;  //Random initial position
+ 	vec v0 = vec(3).randn() * 0.1 * my_trap.d_;  //Random initial velocity
 	
 	Particle my_particle( 1 , 40.078 , r0 , v0);
 
@@ -40,13 +40,16 @@ int main(){
 	
 	my_trap.add_particle(my_particle);
 
+
 	
-	int n = 1;
 	
 	//We define two matrices that will contain the three components of the velocity and position of the particle
+	
 	mat Re, Ve;
 
+	
 	//Now we fill this matrices with the initial values of the position and the velocity
+	
 	Re << r0[0] << endr
 	   << r0[1] << endr
 	   << r0[2];
@@ -56,12 +59,21 @@ int main(){
 	   << v0[2];
 
 	
+	
+	
+	//We now define a counter for the step
+	
+	int n = 1;
+	
+	
 	//With this loop, while the time is below the final time defined, the Forward Euler algorithm will evolve the Penning Trap system in time
+	
 	while (t <= tf) {
 
 		my_trap.evolve_forward_Euler(h, 0, t, f, w, true, false, true);
 		
 		//We insert in the matrices previously defined the different values of the position and velocity, values calculated thanks to the Forward Euler method
+		
 		Re.insert_cols(n, my_trap.particle_collection[0].r_);
 		Ve.insert_cols(n, my_trap.particle_collection[0].v_);
 
@@ -71,7 +83,9 @@ int main(){
 
 	}
 
-	//We create two .txt files that stores  respectively the matrices that contains the values of the position and velocity of the particle
+	
+	//We create two .txt files that stores respectively the matrices that contains the values of the position and velocity of the particle
+	
 	ofstream ofile1;
         ofile1.open("Problem9a_Re.txt");
 	ofile1 << scientific;
