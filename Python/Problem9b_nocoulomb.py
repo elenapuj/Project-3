948b326aacddec1cf6f3cd2e9172ea686817d11b
mariaperez01 <89605350+mariaperez01@users.noreply.github.com>
2021-10-19 19:06:34 +0200

    Create Problem9b_nocoulomb.py

---
+++ Python/Problem9b_nocoulomb.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 18:28:12 2021
+
+@author: María Pérez
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List
+
+"""
+The code opens two data files, creates three vectors for each file, and fills them with the three 
+components of the position of each particle when the Coulomb interactions are switched off
+"""
+with  open('NoCoulomb_r1.txt', "r") as  infile:
+    
+    lines = infile.readlines ()
+
+    x_1: List[float] = []
+    y_1: List[float] = []
+    z_1: List[float] = []
+    
+    for  line in  lines:
+        vals = line.split()
+        x_1.append(float(vals [0]))
+        y_1.append(float(vals [1]))
+        z_1.append(float(vals [2]))
+        
+with  open('NoCoulomb_r2.txt', "r") as  infile:
+    lines = infile.readlines ()
+
+    x_2: List[float] = []
+    y_2: List[float] = []
+    z_2: List[float] = []
+    
+    for  line in  lines:
+        vals1 = line.split()
+        x_2.append(float(vals1 [0]))
+        y_2.append(float(vals1 [1]))
+        z_2.append(float(vals1 [2]))
+ 
+#Plot
+plt.plot(x_1, y_1, color = 'red', label = 'Motion of the particle 1 in the x-y plane')
+plt.legend(loc = "lower left")
+plt.ylabel("y (μm)")
+plt.xlabel("x (μm)")
+plt.xlim([-2500, 2500])
+plt.ylim([-2500, 2500])
+plt.grid(True)
+
+plt.plot(x_2, y_2, color = 'blue', label = 'Motion of the particle 2 in the x-y plane')
+plt.legend(loc = "lower left")
+plt.ylabel("y (μm)")
+plt.xlabel("x (μm)")
+plt.xlim([-2500, 2500])
+plt.ylim([-2500, 2500])
+plt.grid(True)
+
+#The graph is saved in a PDF file
+plt.savefig("Problem_9b_plot_nocoulomb.pdf")
