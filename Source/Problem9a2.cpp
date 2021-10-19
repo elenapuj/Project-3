6be90972ee3bade361446fb76c96bd562b121041
mariaperez01 <89605350+mariaperez01@users.noreply.github.com>
2021-10-19 10:14:35 +0200

    Update Problem9a2.cpp

--- Source/Problem9a2.cpp
+++ Source/Problem9a2.cpp
@@ -1,16 +1,20 @@
 #include "PenningTrap.hpp"
 
 int main(){
-
+	
+	//The PenningTrap holds a collection of particle
 	vector<Particle> my_particle_collection;
 
+	//We create an specific trap by calling the PenningTrap constructor
 	PenningTrap my_trap( 96.5, 9.65 * pow(10 , 8), 10000 , my_particle_collection);
 
 	vec r0 = vec(3).randn() * 0.1 * my_trap.d_;  // random initial position
  	vec v0 = vec(3).randn() * 0.1 * my_trap.d_;  // random initial velocity
 
+	//We create a particle by calling the Particle constructor
 	Particle my_particle( 1 , 40.078 , r0 , v0);
 
+	//Here we add the particle mentioned before in the specific Penning trap
 	my_trap.add_particle(my_particle);
 
 	double f = 0;
@@ -24,9 +28,11 @@ int main(){
 	double tf = 100; //final time
 
 	int n = 1;
-
+	
+	//We define two matrices that will contain the three components of the velocity and position of the particle
 	mat Re, Ve;
 
+	//Now we fill this matrices with the initial values of the position and the velocity
 	Re << r0[0] << endr
 	   << r0[1] << endr
 	   << r0[2];
@@ -35,11 +41,13 @@ int main(){
 	   << v0[1] << endr
 	   << v0[2];
 
-
+	
+	//With this loop, while the time is below the final time defined, the Forward Euler algorithm will evolve the Penning Trap system in time
 	while (t <= tf) {
 
 		my_trap.evolve_forward_Euler(h, 0, t, f, w, true, false, true);
-
+		
+		//We insert in the matrices previously defined the different values of the position and velocity, values calculated thanks to the Forward Euler method
 		Re.insert_cols(n, my_trap.particle_collection[0].r_);
 		Ve.insert_cols(n, my_trap.particle_collection[0].v_);
 
@@ -49,7 +57,7 @@ int main(){
 
 	}
 
-
+	//We create two .txt files that stores  respectively the matrices that contains the values of the position and velocity of the particle
 	ofstream ofile1;
         ofile1.open("Problem9a_Re.txt");
 	ofile1 << scientific;
