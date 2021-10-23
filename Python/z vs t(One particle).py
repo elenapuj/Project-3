bd0fee226e979f3e5839c4f24a0d82c3eb683345
Iván Villegas Pérez <89591182+ivanvillegas7@users.noreply.github.com>
2021-10-23 19:35:44 +0200

    Update z vs t(One particle).py

--- Python/z vs t(One particle).py
+++ Python/z vs t(One particle).py
@@ -6,7 +6,7 @@ from typing import List
 We open two data files, create three vectors for each file, and fill them with the three 
 components of the position and velocity of the particle respectively
 """
-with  open('Problem9a_Rrk.txt', "r") as  infile:
+with  open('1part R RK.txt', "r") as  infile:
     
     lines = infile.readlines ()
 
@@ -20,7 +20,7 @@ with  open('Problem9a_Rrk.txt', "r") as  infile:
         y.append(float(vals [1]))
         z.append(float(vals [2]))
         
-with  open('Problem9a_Vrk.txt', "r") as  infile:
+with  open('1part V RK.txt', "r") as  infile:
     lines = infile.readlines ()
 
     vx: List[float] = []
