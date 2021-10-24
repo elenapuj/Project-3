94d7213b62b88756ce0fbd15a2696bb0e1ababee
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-10-24 21:40:02 +0200

    Update Resonance_2_3.py

--- Python/Resonance_2_3.py
+++ Python/Resonance_2_3.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 
+"""
+We open two data files, create a vector for each file, and fill them with 
+the results of the fraction of particles remaining inside the trap for each 
+angular frequency for a constant amplitude f=0.4 with and without taking into 
+account Coulomb's force
+"""
+
 with  open('Resonance_2.txt', "r") as  infile:
     
     lines = infile.readlines ()
@@ -21,9 +28,12 @@ with  open('Resonance_3.txt', "r") as  infile:
     for  line in  lines:
         vals = line.split()
         coulomb.append(float(vals [0]))
-       
+        
+#The values of the angular frequency (near a resonance)
 w_v = np.linspace(0.25 , 0.75 , 116 )
-plt.figure(figsize=(5,5))
+
+#Plot
+plt.figure(figsize=(5,5)) #Squared plot
 plt.plot(w_v, no_coulomb, label = "Without Coulomb", color = "blue")
 plt.plot(w_v, coulomb, label = "With Coulomb", color = "red") 
 plt.title(r'Fraction of particles (N=100 particles) inside the trap after 500 $\mu s$ vs $\omega_V$', fontsize=10)
@@ -31,5 +41,7 @@ plt.ylabel("Fraction of particles inside" )
 plt.xlabel(r'$\omega_V$ (MHz)') 
 plt.grid(True) #Grids get painted
 plt.legend(loc='right', bbox_to_anchor=(1.46, 0.5), ncol = 1)
+
+#The graph is saved in a PDF file
 plt.savefig("Resonance_2_3.pdf", bbox_inches=('tight'))
 plt.show()
