14c0c5ce8937151a6e4c2627093defecd5d604f9
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-10-24 21:36:54 +0200

    Update Resonance_1.py

--- Python/Resonance_1.py
+++ Python/Resonance_1.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 
+"""
+We open a data file, create three vectors for each file, and fill them with 
+the results of the fraction of particles remaining inside the trap for each 
+angular frequency and the constant amplitude f
+"""
+
 with  open('Resonance_1.txt', "r") as  infile:
     
     lines = infile.readlines ()
@@ -16,8 +22,10 @@ with  open('Resonance_1.txt', "r") as  infile:
         f2.append(float(vals [1]))
         f3.append(float(vals [2]))
         
-     
+#The values of the angular frequency
 w_v = np.linspace(0.2 , 2.5 , 116 )
+
+#Plot
 plt.plot(w_v, f1, label = "f=0.1", color = "red") 
 plt.plot(w_v, f2, label = "f=0.4", color = "blue")
 plt.plot(w_v, f3, label = "f=0.7", color = "green")
@@ -26,6 +34,9 @@ plt.ylabel("Fraction of particles inside" )
 plt.xlabel(r'$\omega_V$ (MHz)') 
 plt.grid(True) #Grids get painted
 plt.legend()
+
+#The graph is saved in a PDF file
 plt.savefig("Resonance_1.pdf")
+
 plt.show()
 
